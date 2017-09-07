0e043035c84480d0c0cf785e92bb9192cdb7efc5
neil1923 <799353209@qq.com>
2017-09-07 13:26:11 +0800

    1.colse:   1.1完成手机验证弹出层的交互 2.open:   2.1cookie计时交互

--- src/main/webapp/jsp/detail.jsp
+++ src/main/webapp/jsp/detail.jsp
@@ -23,7 +23,7 @@
         </div>
     </div>
 </div>
-<%--登录弹出层 输入电话--%>
+<%--登录弹出层 输入电话 采用的是bootstrap的<modal fade>隐藏--%>
 <div id="killPhoneModal" class="modal fade">
 
     <div class="modal-dialog">
@@ -31,7 +31,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h3 class="modal-title text-center">
-                    <span class="glyphicon glyphicon-phone"> </span>秒杀电话:
+                    <span class="glyphicon glyphicon-phone"></span>秒杀电话:
                 </h3>
             </div>
 
@@ -45,7 +45,7 @@
             </div>
 
             <div class="modal-footer">
-                <%--验证信息--%>
+                <%--验证信息手否通过（提示确认信息）--%>
                 <span id="killPhoneMessage" class="glyphicon"> </span>
                 <button type="button" id="killPhoneBtn" class="btn btn-success">
                     <span class="glyphicon glyphicon-phone"></span>
@@ -68,16 +68,24 @@
 <%--jQuery countDown倒计时插件--%>
 <script src="http://cdn.bootcss.com/jquery.countdown/2.1.0/jquery.countdown.min.js"></script>
 
-<script src="/resource/script/seckill.js" type="text/javascript"></script>
+<!-- 本地js文件，前端的各种操作都在这里 -->
+<!-- 部署到服务器上时，这里的src路径要写成../scripts/seckill.js而不是/scripts/seckill.js -->
+<!-- 正确的js网址为  http://127.0.0.1:8080/seckill_imooc/resource/script/seckill.js -->
+<!-- 访问的却是 http://127.0.0.1:8080/seckill_imooc/seckill/resource/script/seckill.js 多了一个seckill的路径！！-->
+<script src="../resource/script/seckill.js" type="text/javascript"></script>
 
 <script type="text/javascript">
+
     $(function () {
-        //使用EL表达式传入参数
+        //使用EL表达式把jsp中的值 传到js中方法的参数
+        //seckill 是从controller中传入的对象
         seckill.detail.init({
             seckillId:${seckill.seckillId},
             startTime:${seckill.startTime.time},//毫秒
             endTime:${seckill.endTime.time}
         });
-    })
+    });
+
+
 </script>
 </html>
--- src/main/webapp/resource/script/seckill.js
+++ src/main/webapp/resource/script/seckill.js
@@ -1,16 +1,76 @@
 //存放主要的交互逻辑的js代码
 //javascript模块化（包.类.方法名）
-
+//seckill.detail.init(params)
 var seckill = {
 
-    //1.封装秒杀相关的ajax的url
+    //1.封装秒杀相关ajax的url
+    URL: {
+        //注意这里的调用的是访问地址，http://127.0.0.1:8080/time/now
+        //要与后端控制器里提供的地址一致，我自己控制器里访问地址是不带seckill的
+        //之前报404错误就是没找到该地址，地址写错了，写成了/seckill/time/now，多了一个seckill
+
+        //部署到服务器上时，这里的路径要写../time/now，不能写成/time/now
+        //否则会找不到路径，貌似js里的路径和有关js的路径都要这样写！！！！
+        now: function () {
+            return '../time/now';
+        },
+        //暴露秒杀地址
+        exposer: function (seckillId) {
+            return '../' + seckillId + '/exposer';
+        },
+        //执行秒杀
+        execution: function (seckillId, md5) {
+            return '../' + seckillId + '/' + md5 + '/execution';
+        }
+    },
 
     //2.验证手机号
+    validatePhone: function (phone) {
+        //直接判断对象会看对象是否为空,空就是undefine就是false; isNaN 非数字返回true
+        if (phone && phone.length == 11 && !isNaN(phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
 
     //3.详情页秒杀逻辑
+    detail: {
+        //详情页初始化
+        init: function (params) {
+            //手机验证和登录,计时交互
+            //规划我们的交互流程
+            //在cookie中查找手机号
+            var userPhone = $.cookie('userPhone');
+            //验证手机号
+            if (!seckill.validatePhone(userPhone)) {
+                //绑定手机 控制输出
+                var killPhoneModal = $('#killPhoneModal');
+                killPhoneModal.modal({
+                    show: true,//显示弹出层
+                    backdrop: false,//禁止位置关闭
+                    keyboard: false//关闭键盘事件
+                });
 
-    //4.获取秒杀地址 控制显示器 执行秒杀
+                $('#killPhoneBtn').click(function () {
+                    var inputPhone = $('#killPhoneKey').val();
+                    console.log("inputPhone: " + inputPhone);
+                    if (seckill.validatePhone(inputPhone)) {
+                        //电话写入cookie(1天过期)
 
-    //5.倒计时逻辑
+                        $.cookie('userPhone', inputPhone, {expires: 1});
+                        //验证通过　　刷新页面
+                        window.location.reload();
+                    } else {
+                        //todo 错误文案信息抽取到前端字典里
+                        $('#killPhoneMessage').hide().html('<label class="label label-danger">手机号错误!</label>').show(300);
+                    }
+                });
+            }
+        }
+    }
+
+
+
 
 }
